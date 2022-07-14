588777e37b9e63325bf69fe3db4f2f06a5f64543
nakaros <nakalabpublic@gmail.com>
2022-07-14 13:44:12 +0900

    整理

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # crane_x7_deflection_estimation_model
+
+- estimate_deflection.py 
+
+　　取得したデータからたわみ量を学習するプログラム
+
+- model.pth 
+
+　　学習済みモデル
+
+- train_data.csv
+
+　　取得した学習データが保存されているファイル
--- estimate_deflection.py
+++ estimate_deflection.py
@@ -1,11 +1,11 @@
+import torch
 from torch import nn, optim
 import numpy as np
-from matplotlib import pyplot as plt
-import torch
 import pandas as pd
+from matplotlib import pyplot as plt
  
 # 線形回帰ネットワークのclassをnn.Moduleの継承で定義
-class Estimate_defle1(nn.Module):
+class DeflectionEstimator(nn.Module):
     
     def __init__(self):
         super().__init__()
@@ -13,58 +13,41 @@ class Estimate_defle1(nn.Module):
         self.sigmoid = nn.Sigmoid()
         self.linear2 = nn.Linear(10,2)
 
-    def forward(self, datass):
-        datass = self.linear1(datass)
-        datass = self.sigmoid(datass)
-        datass = self.linear2(datass)
+    def forward(self, data):
+        data = self.linear1(data)
+        data = self.sigmoid(data)
+        data = self.linear2(data)
+        return data
 
-        return datass
-    
-    # きちんと学習できてるか確認用
-    def test(self,z1,z2):
-        z = torch.Tensor([z1,z2])
-        out = net(z)/100
-        return out
+#学習データの読み込み
+df = pd.read_csv(filepath_or_buffer="train_data.csv")
 
-def test(z1,z2):
-    z = torch.Tensor([z1,z2])
-    out = net(z)/100
-    return out
-
-#データの読み込み
-df = pd.read_csv(filepath_or_buffer="/home/nakalab/デスクトップ/ebara_crane/estimate_deflection/res0511.csv")
-
-#指令値のz座標
+#指令値（学習データ）のz座標と目標までの距離
 data = torch.Tensor( [df.iloc[:,7],df.iloc[:,3]])
-datas = torch.t(data)
-datass = torch.tensor(datas, requires_grad=True)
+data = torch.t(data)
+datas = torch.tensor(data, requires_grad=True)
 
-#ARマーカーのz座標
+#計測するARマーカー（ラベルデータ）のz座標と目標までの距離
 label = torch.Tensor([df.iloc[:,8],df.iloc[:,6]])
-labels = torch.t(label)
-labelss = torch.tensor(labels, requires_grad=True)
+label = torch.t(label)
+labels = torch.tensor(label, requires_grad=True)
 
 # データ数
-N = len(datass)
+N = len(datas)
 
-net = Estimate_defle1()
+net = DeflectionEstimator()
 optimizer = torch.optim.Adam( net.parameters(), lr=0.001 )
 
-predicts = net(datass)
-
+#学習
 loss_list = []
 for i in range(20000):
     optimizer.zero_grad()
-    predicts = net(datass)
-    loss = torch.sqrt(1/N*torch.sum((labelss - predicts)**2))  
+    predicts = net(datas)
+    loss = torch.sqrt(1/N*torch.sum((labels - predicts)**2))  
     loss.backward()
     optimizer.step()
     print(loss)
-    loss_list.append( loss.detach().numpy())
 
-plt.plot( range(len(loss_list)), loss_list ) 
-plt.savefig("result.png")  
-
-#モデルを保存
+#学習したモデルを保存
 net = net.to('cpu')
-torch.save(net.state_dict(), 'model_cpu_05111.pth')
+torch.save(net.state_dict(), 'model.pth')
